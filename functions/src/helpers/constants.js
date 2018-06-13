a33488de30092a4455d7ea5b0c40deeb3b2470eb
Wilson Daniel Ospina <wdospinal@unal.edu.co>
2018-06-13 00:37:51 -0500

    chore: add contasts for manage response in functions

---
+++ functions/src/helpers/constants.js
@@ -0,0 +1,13 @@
+const errorCodes = {
+  1: 'Error in firebase server',
+  10: 'No implemented yet',
+};
+const PATH_USER = 'users/';
+const PATH_MESSAGE = 'messages/';
+
+module.exports = {
+  errorCodes,
+  PATH_USER,
+  PATH_MESSAGE,
+};
+
